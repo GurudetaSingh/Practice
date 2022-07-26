35b79f55b6a49872d17a7a4d3e45327f15c38c8f
Gurudeta Singh <Gurudetasingh@gmail.com>
2022-07-26 03:47:06 -0700

    Create Count_Rotations.py

---
+++ Count_Rotations.py
@@ -0,0 +1,12 @@
+# You are given list of numbers, obtained by rotating a sorted list an unknown number of times. 
+# Write a function to determine the minimum number of times the original sorted list was rotated to obtain the given list. 
+def count_rotations(nums):
+    position = 1 
+    
+    while position < len(nums):
+        if position > 0 and nums[position] < nums[position-1]:  
+            return position
+        
+        position += 1
+        
+    return -1
